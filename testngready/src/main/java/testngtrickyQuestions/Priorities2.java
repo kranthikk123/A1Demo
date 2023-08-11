a33b9316791302c04871a882f129ee3d581a38bd
KranthiKumar <kranthikumarcse233@gmail.com>
2023-08-11 17:49:04 +0530

    this commit

---
+++ testngready/src/main/java/testngtrickyQuestions/Priorities2.java
@@ -0,0 +1,34 @@
+package testngtrickyQuestions;
+
+import org.testng.annotations.Test;
+
+public class Priorities2 {
+
+	@Test()
+	public void a() {
+		System.out.println("This is a with no priority");
+	}
+
+	@Test(priority = 1)
+	public void b() {
+		System.out.println("This is b with priority 1");
+	}
+
+	@Test(priority = -1)
+	public void c() {
+		System.out.println("This is c with prioriy -1");
+	}
+
+	@Test(priority = 0)
+	public void d() {
+		System.out.println("This is d with prioiry 0");
+	}
+	@Test(priority = 2)
+	public void e() {
+		System.out.println("This is d with prioiry  2");
+	}
+	@Test(priority =32 )
+	public void f() {
+		System.out.println("This is d with prioiry 3");
+	}
+}
